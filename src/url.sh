d243a3cc7eec7311605d4f390bd3f464fadefe1b
Marcus Birkenkrahe <birkenkrahe@lyon.edu>
2023-04-19 11:39:45 -0500

    updates

--- src/url.sh
+++ src/url.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 spotifyzip='https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip'
-coffee='https://github.com/birkenkrahe/ds2/blob/main/data/coffee.xlsx'
+coffee='https://github.com/birkenkrahe/ds2/blob/main/data/coffee.xlsx?raw=true'
 spotify='https://github.com/birkenkrahe/ds2/blob/main/data/Spotify_data.xlsx?raw=true'
